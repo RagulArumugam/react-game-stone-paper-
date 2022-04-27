63d032eec74e183bd97cebef9079c970a6510077
Ragul Arumugam <ragul.arumugam@cesltd.com>
2022-04-27 20:06:27 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext } from "react";
+import "./App.css";
+import ScoreBoard from "./components/scoreBoard/scoreBoard";
+import OptionContainer from "./components/optionsContainer/optionsContainer";
+import GameArea from "./components/gameArea/gameArea";
+import { mainContext } from "./context/mainContext";
 
 function App() {
+  const { score } = useContext(mainContext);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ScoreBoard score={score} />
+      {/* <OptionContainer /> */}
+      <GameArea />
     </div>
   );
 }
